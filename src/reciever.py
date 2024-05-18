85935964f08f1accc95a8f9e407df091ae630323
Tonypina <pina.rossette@comunidad.unam.mx>
2024-05-18 10:02:41 -0600

    IR reciever

--- src/reciever.py
+++ src/reciever.py
@@ -2,8 +2,9 @@ import pigpio
 import time
 
 class IRReceiver:
-    def __init__(self, gpio_pin):
+    def __init__(self, gpio_pin, file_name):
         self.gpio_pin = gpio_pin
+        self.file_name = file_name
         self.pi = pigpio.pi()
         self.pi.set_mode(self.gpio_pin, pigpio.INPUT)
         self.pi.set_glitch_filter(self.gpio_pin, 100)
@@ -22,19 +23,23 @@ class IRReceiver:
             if self.in_code:
                 self.data.append(pigpio.tickDiff(self.last_tick, tick))
                 self.in_code = False
-                self.print_data()
+                self.save_data()
         self.last_tick = tick
 
-    def print_data(self):
+    def save_data(self):
         if len(self.data) > 0:
-            print("Raw data:", self.data)
+            with open(self.file_name, 'a') as file:
+                file.write(','.join(map(str, self.data)) + '\n')
+                file.write('-----------\n')
+            print("Data saved:", self.data)
 
     def stop(self):
         self.pi.stop()
 
 if __name__ == "__main__":
     gpio_pin = 17  # Reemplaza con el pin GPIO al que está conectado el VS1838B
-    receiver = IRReceiver(gpio_pin)
+    file_name = 'ir_data.txt'  # Nombre del archivo donde se guardarán los datos
+    receiver = IRReceiver(gpio_pin, file_name)
     try:
         while True:
             time.sleep(1)
