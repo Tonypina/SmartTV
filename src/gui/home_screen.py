da46c489f6d30f4317134950ec47c691d801eb3d
Tonypina <pina.rossette@comunidad.unam.mx>
2024-05-19 13:23:50 -0600

    Se agregó botón con imagen

--- src/gui/home_screen.py
+++ src/gui/home_screen.py
@@ -8,15 +8,18 @@ class HomeScreen():
 
     def __init__(self, panel_principal, app_logic):
         
-        button_width = 500
-        button_height = 500
+        button_width = 200
+        button_height = 200
         button_font = font.Font(family='FontAwesome', size=15)
 
         self.buttonNetflix = tk.Button( panel_principal )
-        self.nextflixImg = util_img.leer_imagen("./../src/img/Netflix.png", (500, 500))
+        self.nextflixImg = util_img.leer_imagen("./../src/img/Netflix.png", (200, 200))
+        self.buttonGoogle = tk.Button( panel_principal )
+        self.googleImg = util_img.leer_imagen("./../src/img/Google.png", (200, 200))
 
         buttons_info = [
             ("Netflix", self.nextflixImg, self.buttonNetflix, app_logic.open_netflix_kiosk)
+            ("Google", self.googleImg, self.buttonGoogle, app_logic.open_google_kiosk)
         ]
 
         for text, img, button, command in buttons_info:
@@ -25,6 +28,7 @@ class HomeScreen():
     def buttons_config(self, text, img, button, button_font, button_width, button_height, command):
         button.config(text=f"{text}", image=img, anchor="center", font=button_font,
                       bd=0, bg=COLOR_MENU_LATERAL, fg="white", width=button_width, height=button_height,
+                      highlightthickness = 0,
                       command = command)
         button.pack(side=tk.LEFT, expand=True)
         self.bind_hover_events(button)
