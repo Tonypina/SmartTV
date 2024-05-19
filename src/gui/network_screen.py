d19a4995a8b27f4fc983a9f89e0146db0cb0952a
Tonypina <pina.rossette@comunidad.unam.mx>
2024-05-19 15:31:44 -0600

    Selección de red

--- src/gui/network_screen.py
+++ src/gui/network_screen.py
@@ -31,7 +31,7 @@ class NetworkScreen():
         self.selectedSSIDLabel = tk.Label(self.barra_der, text="")
         self.selectedSSIDLabel.config(fg="#222d33", font=(
             "Roboto", 20), padx=10, width=20, bg=COLOR_CUERPO_PRINCIPAL)
-        self.selectedSSIDLabel.pack(side=tk.TOP, expand=True)
+        self.selectedSSIDLabel.pack(side=tk.TOP)
 
         for network in self.wifi_ssids:
             self.ssids_config(network)
@@ -39,7 +39,7 @@ class NetworkScreen():
     def ssids_config(self, ssid):
         ssidLabel = tk.Button(self.barra_izq, font=(
             "Roboto", 20), text=ssid, anchor="w", bd=0, fg="#222d33", command=self.select_ssid(ssid), 
-            highlightthickness = 0, bg=COLOR_CUERPO_PRINCIPAL, pady=10, expand=True, )
+            highlightthickness = 0, bg=COLOR_CUERPO_PRINCIPAL, pady=10)
         ssidLabel.pack()
 
     def select_ssid(self, ssid):
