db82d52f7843f45cd9f4b8370f70a8b97a051894
Tonypina <pina.rossette@comunidad.unam.mx>
2024-05-20 12:34:08 -0600

    Video

--- src/gui/video_options_screen.py
+++ src/gui/video_options_screen.py
@@ -20,30 +20,29 @@ class VideoScreen:
         self.barra_sup = tk.Frame(panel_principal)
         self.barra_sup.pack(side=tk.TOP, fill=tk.X, expand=False)
 
-        # Crear paneles: barra derecha
+        # Crear paneles: barra inferior
         self.barra_inf = tk.Frame(panel_principal)
-        self.barra_inf.pack(side=tk.BOTTOM, fill=tk.X, expand=True)
+        self.barra_inf.pack(side=tk.TOP, fill=tk.X, expand=True)
 
         # Título
         self.labelTitulo = tk.Label(self.barra_sup, text="Selecciona un Video")
         self.labelTitulo.config(fg="#222d33", font=("Roboto", 30), pady=50)
         self.labelTitulo.pack(side=tk.TOP, fill='both', expand=True)
 
-        # Lista de videos
-        self.video_listbox = tk.Listbox(self.barra_inf, font=("Roboto", 20))
-        self.video_listbox.pack(side=tk.TOP, fill="both", expand=True)
+        # Crear botones para cada video
         for video in self.video_files:
-            self.video_listbox.insert(tk.END, video)
+            self.create_video_button(video)
 
-        # Botón de reproducir
-        self.play_button = tk.Button(self.barra_inf, text="Reproducir", font=("Roboto", 20), command=self.abrir_video_frame)
-        self.play_button.pack(side=tk.BOTTOM, fill=tk.X, expand=False)
+    def create_video_button(self, video):
+        button = tk.Button(self.barra_inf, text=video, font=("Roboto", 20), command=lambda: self.abrir_video_frame(video))
+        button.pack(fill=tk.X, pady=5)
 
     def limpiar_panel(self, panel):
-    # Función para limpiar el contenido del panel
+        # Función para limpiar el contenido del panel
         for widget in panel.winfo_children():
             widget.destroy()
 
-    def abrir_video_frame(self):
-        self.limpiar_panel(self.panel_principal)     
-        VideoFrame(self.panel_principal, self.app_logic, self.video_files[self.video_listbox.curselection()])
+    def abrir_video_frame(self, video):
+        self.limpiar_panel(self.panel_principal)
+        VideoFrame(self.panel_principal, self.app_logic, video)
+
